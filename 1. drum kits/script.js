a8d1af40ed8a3cd3d9ab7f81eb8779b6510da0c0
leonardoj6 <l.jun1717@gmail.com>
2018-10-29 00:26:51 -0600

    finished

--- 1. drum kits/script.js
+++ 1. drum kits/script.js
@@ -1,5 +1,22 @@
-window.addEventListener("keypress", e => {
+// window.addEventListener("keypress", e => {
+//   const keyCode = e.charCode;
+//   //   console.log(keyCode);s
+//   const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+//   if (!audio) {
+//     return;
+//   }
+//   /** play audio */
+//   audio.currentTime = 0;
+//   audio.play();
+//   /** animation of key */
+//   const keySelected = document.querySelector(`.key[data-key="${keyCode}"]`);
+//   //   console.log(keySelected);
+//   keySelected.classList.add("playing");
+// });
+
+function addTransistion(e) {
   const keyCode = e.charCode;
+  //   console.log(keyCode);s
   const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
   if (!audio) {
     return;
@@ -11,7 +28,18 @@ window.addEventListener("keypress", e => {
   const keySelected = document.querySelector(`.key[data-key="${keyCode}"]`);
   //   console.log(keySelected);
   keySelected.classList.add("playing");
-});
+}
 
-const keyList = document.querySelectorAll(".key");
-console.log(keyList.length);
+function removeTransition(e) {
+  if (e.propertyName != "transform") {
+    return;
+  }
+  e.target.classList.remove("playing");
+}
+
+var keyList = document.querySelectorAll(".key");
+keyList.forEach(key => {
+  //   console.log(key);
+  key.addEventListener("transitionend", removeTransition);
+});
+window.addEventListener("keypress", addTransistion);
